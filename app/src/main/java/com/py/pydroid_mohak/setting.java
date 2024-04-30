cdc108f2f0c5ab2e0ea672a500f7b8a381229748
lukiod <mohakgupta0981@gmail.com>
2024-04-30 15:26:30 +0530

    setting done

--- app/src/main/java/com/py/pydroid_mohak/setting.java
+++ app/src/main/java/com/py/pydroid_mohak/setting.java
@@ -1,64 +1,61 @@
 package com.py.pydroid_mohak;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link setting#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class setting extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public setting() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment setting.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static setting newInstance(String param1, String param2) {
-        setting fragment = new setting();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FirebaseAuth mAuth;
+    private Button profileButton;
+    // ... other button declarations ...
+    private Button signOutButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        // Find view references
+        profileButton = view.findViewById(R.id.profileButton);
+        // ... other findViewById calls ...
+        signOutButton = view.findViewById(R.id.signout); // Make sure the ID matches your button in XML
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Set onClickListener for profile button
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Profile activity
+                Intent intent = new Intent(getActivity(), Profile.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set onClickListener for sign-out button
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOutUser();
+            }
+        });
+
+        return view;
+    }
+
+    private void signOutUser() {
+        mAuth.signOut();
+        // Navigate the user to the login screen or another relevant activity
+        Intent intent = new Intent(getActivity(), Login.class); // Replace with your LoginActivity
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        // Optionally, finish the current activity
+        getActivity().finish();
     }
 }
